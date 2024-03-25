578abd4423a743d96ca01f6cbb92edef0418db9e
Bilal-2 <107888984+Bilal-2@users.noreply.github.com>
2024-03-26 02:39:22 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -116,7 +116,7 @@ mongoDB();
 // Use cors middleware with specific origin
 //Use cors middleware with specific origin
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: '*'
 }));
 
 const conn = mongoose.connection;

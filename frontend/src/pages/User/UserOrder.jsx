1f82f58623c2c44f7047cbb3624fb391a6d32717
Bilal-2 <107888984+Bilal-2@users.noreply.github.com>
2024-04-11 11:36:09 +0500

    Update UserOrder.jsx

--- frontend/src/pages/User/UserOrder.jsx
+++ frontend/src/pages/User/UserOrder.jsx
@@ -7,77 +7,96 @@ const UserOrder = () => {
   const { data: orders, isLoading, error } = useGetMyOrdersQuery();
 
   return (
-    <div className="container mx-auto">
-      <h2 className="text-2xl font-semibold mb-4">My Orders </h2>
-
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">{error?.data?.error || error.error}</Message>
+    <>
+      {orders && orders.length === 0 ? (
+        <div className="p-5 flex w-full justify-center flex-wrap flex-col">
+          <div className="p-5 flex w-full justify-center flex-wrap ">
+            <h2 className="text-2xl font-semibold mb-4">Nothing in Orders </h2>
+          </div>
+          <div className="p-5 flex w-full justify-center flex-wrap ">
+            <Link to="/shop">
+              <button className="bg-[#9f5e1d] hover:bg-[#6f3914] text-back py-2 px-3 rounded">
+                SHOP ITEMS
+              </button>
+            </Link>
+          </div>
+        </div>
       ) : (
-        <table className="w-full">
-          <thead>
-            <tr>
-              <td className="py-2">IMAGE</td>
-              <td className="py-2">ID</td>
-              <td className="py-2">DATE</td>
-              <td className="py-2">TOTAL</td>
-              <td className="py-2">PAID</td>
-              <td className="py-2">DELIVERED</td>
-              <td className="py-2"></td>
-            </tr>
-          </thead>
+        <div className="container mx-auto">
+          <h2 className="text-2xl font-semibold mb-4">My Orders </h2>
 
-          <tbody>
-            {orders.map((order) => (
-              <tr key={order._id}>
-                <img
-                  src={order.orderItems[0].image}
-                  alt={order.user}
-                  className="w-[6rem] mb-5"
-                />
+          {isLoading ? (
+            <Loader />
+          ) : error ? (
+            <Message variant="danger">
+              {error?.data?.error || error.error}
+            </Message>
+          ) : (
+            <table className="w-full">
+              <thead>
+                <tr>
+                  <td className="py-2">IMAGE</td>
+                  <td className="py-2">ID</td>
+                  <td className="py-2">DATE</td>
+                  <td className="py-2">TOTAL</td>
+                  <td className="py-2">PAID</td>
+                  <td className="py-2">DELIVERED</td>
+                  <td className="py-2"></td>
+                </tr>
+              </thead>
 
-                <td className="py-2">{order._id}</td>
-                <td className="py-2">{order.createdAt.substring(0, 10)}</td>
-                <td className="py-2">Rs  {order.totalPrice}</td>
+              <tbody>
+                {orders.map((order) => (
+                  <tr key={order._id}>
+                    <img
+                      src={order.orderItems[0].image}
+                      alt={order.user}
+                      className="w-[6rem] mb-5"
+                    />
 
-                <td className="py-2">
-                  {order.isPaid ? (
-                    <p className="p-1 text-center bg-green-400 w-[6rem] rounded-full">
-                      Completed
-                    </p>
-                  ) : (
-                    <p className="p-1 text-center bg-red-400 w-[6rem] rounded-full">
-                      Pending
-                    </p>
-                  )}
-                </td>
+                    <td className="py-2">{order._id}</td>
+                    <td className="py-2">{order.createdAt.substring(0, 10)}</td>
+                    <td className="py-2">Rs {order.totalPrice}</td>
 
-                <td className="px-2 py-2">
-                  {order.isDelivered ? (
-                    <p className="p-1 text-center bg-green-400 w-[6rem] rounded-full">
-                      Completed
-                    </p>
-                  ) : (
-                    <p className="p-1 text-center bg-red-400 w-[6rem] rounded-full">
-                      Pending
-                    </p>
-                  )}
-                </td>
+                    <td className="py-2">
+                      {order.isPaid ? (
+                        <p className="p-1 text-center bg-green-400 w-[6rem] rounded-full">
+                          Completed
+                        </p>
+                      ) : (
+                        <p className="p-1 text-center bg-red-400 w-[6rem] rounded-full">
+                          Pending
+                        </p>
+                      )}
+                    </td>
 
-                <td className="px-2 py-2">
-                  <Link to={`/order/${order._id}`}>
-                    <button className="bg-[#9f5e1d] hover:bg-[#6f3914] text-back py-2 px-3 rounded">
-                      View Details
-                    </button>
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                    <td className="px-2 py-2">
+                      {order.isDelivered ? (
+                        <p className="p-1 text-center bg-green-400 w-[6rem] rounded-full">
+                          Completed
+                        </p>
+                      ) : (
+                        <p className="p-1 text-center bg-red-400 w-[6rem] rounded-full">
+                          Pending
+                        </p>
+                      )}
+                    </td>
+
+                    <td className="px-2 py-2">
+                      <Link to={`/order/${order._id}`}>
+                        <button className="bg-[#9f5e1d] hover:bg-[#6f3914] text-back py-2 px-3 rounded">
+                          View Details
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
